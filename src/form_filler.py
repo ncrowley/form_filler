5c910dbbc9b1f89c7bec51873b49d9b57e287029
ncrowley <nicholas.l.crowley@gmail.com>
2024-03-08 19:12:19 -0500

    Got a bunch of stuff working -Command line parsing -CSV input -No more global placeholders

--- src/form_filler.py
+++ src/form_filler.py
@@ -1,68 +1,158 @@
 import argparse
+from collections import Counter
+import csv
 from jinja2 import (
     FileSystemLoader,
     Environment,
     meta,
 )
+import os
 
 """
 Current goals:
-    scan a template and determine the require fields that need to be populated
-    take input data from console and build a single output from a template
+    allow user to specify output file name variables
 Long term goals:
-    take input data from a CSV and build multiple outputs
+    maybe break argument stuff out into another module
 """
-
-template_dir = "../templates"
-template_path = "example.template.txt"
-output_path = "../output/example.output.txt"
 
 
 def main():
-    # Init jinja environment
-    loader = FileSystemLoader(template_dir)
-    environment = Environment(loader=loader)
+    parser = build_parser()
+    args = vars(parser.parse_args())
+    data_path = None if not "data_src" in args.keys() else args["data_src"]
+    output_path = args["output"]
+    template_dir = os.path.split(args["template"].name)[0]
+    template_file = os.path.split(args["template"].name)[1]
 
-    # Get a list of fields that need to be populated
-    template_source = environment.loader.get_source(environment, template_path)
-    parsed_content = environment.parse(template_source)
-    undeclared_vars = meta.find_undeclared_variables(parsed_content)
+    environment = init_jinja(template_dir)
 
     # Start a loop to get input from user for those fields
-    var_dict = {}
-    i = 0
-    for item in undeclared_vars:
-        i += 1
-        var_dict[item] = input(f"Give me your cookies {item}: ")
-        # Take user input here
-        print("item: ", item)
-        print("dict: ", var_dict)
-    print("undecls: ", undeclared_vars)
+    undeclared_vars = get_undeclared_vars(environment, template_file)
+    input_data = build_input_data(data_path, undeclared_vars)
 
+    template = environment.get_template(template_file)
     # Now feed those values back in to the template and output to the output file
-    template = environment.get_template(template_path)
-    rendering = template.render(var_dict)
-    print("rendering: ", rendering)
+    create_output(template, input_data, output_path)
+    print("Execution completed successfully.")
 
 
-# To be finished, let's get jinja parsing working and file output working first
+def init_jinja(template_dir):
+    # Init jinja environment
+    return Environment(loader=FileSystemLoader(template_dir))
+
+
+def get_csv_data(data_path):
+    retval = []
+    with open(data_path.name, newline="") as csvfile:
+        reader = csv.reader(csvfile, delimiter=",")
+        for row_num, row in enumerate(reader):
+            retval.append(row)
+    return retval
+
+
+def create_output(template, input_data, output_path):
+    print("Writing output...")
+    var_dict = {}
+    output_path_split = os.path.splitext(output_path)
+    for item in input_data[0]:
+        var_dict[item] = None
+    for data_num, data in enumerate(input_data[1:]):
+        for item_num, item in enumerate(data):
+            var_dict[input_data[0][item_num]] = item
+        rendered_string = template.render(var_dict)
+        # Slide the number in just before the extension
+        output_filename = (
+            f"{output_path_split[0]}{data_num + 1}{output_path_split[1]}"
+        )
+        print("Writing row ", data_num + 1, " to ", output_filename)
+        with open(output_filename, "w") as file:
+            file.write(rendered_string)
+    print("Output complete")
+
+
+def get_undeclared_vars(environment, template_file):
+    # Get a list of fields that need to be populated
+    template_source = environment.loader.get_source(environment, template_file)
+    parsed_content = environment.parse(template_source)
+    return meta.find_undeclared_variables(parsed_content)
+
+
+def build_input_data(data_path, undeclared_vars):
+    retval = []
+    if data_path:
+        retval = get_csv_data(data_path)
+        found_vars = []
+        for item in retval[0]:
+            if item in undeclared_vars:
+                found_vars.append(item)
+        duplicate_column_headers = [
+            k for k, v in Counter(retval[0]).items() if v > 1
+        ]
+        # TODO: Do this in a pythonic way throwing exceptions
+        if duplicate_column_headers:
+            # warning
+            print("duplicate column name(s): ", duplicate_column_headers)
+            print("first column bearing the name will be used")
+        elif set(undeclared_vars) != set(found_vars):
+            # fatal
+            print("missing var(s): ", (set(undeclared_vars) - set(found_vars)))
+            quit()
+        elif set(retval[0]) != set(found_vars):
+            # warning
+            print("unassigned var(s): ", (set(retval[0]) - set(found_vars)))
+        else:
+            print(
+                "Sucessfully mapped data headers to required fields for template."
+            )
+    else:
+        retval = []
+        retval.append(undeclared_vars)
+        i = 1
+        while True:
+            retval.append([None * len(retval[0])])
+            for item_num, item in enumerate(undeclared_vars):
+                retval[i][item_num] = input(f"Enter value for {item}: ")
+            response = None
+            while response and (response != "y" or response != "n"):
+                response = input("Enter another set of input? y/n: ")
+                response = response.lower()
+            if response == "n":
+                break
+            i += 1
+    return retval
+
+
 def build_parser():
     parser = argparse.ArgumentParser(description="Process some templates")
     parser.add_argument(
-        "template",
-        metavar="t",
+        "--data-src",
+        "-d",
+        type=argparse.FileType("r"),
+        help="Input data csv",
+        required=False,
+    )
+    parser.add_argument(
+        "--template",
+        "-t",
         type=argparse.FileType("r"),
         help="The jinja template file",
         required=True,
     )
+    # Can't validate this until we have data to sub into tags
     parser.add_argument(
-        "output_filename",
-        metavar="o",
-        type=argparse.FileType("w"),
-        help="The output file destination",
+        "--output",
+        "-o",
+        help="The output file location, tags can be used to create unique filenames / paths for the output.",
         required=True,
     )
     return parser
+
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
 
 
 if __name__ == "__main__":
